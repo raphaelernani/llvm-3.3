34279769828d5037ec93523bc54797b8d82492c0
raphael <raphaelernani@gmail.com>
2014-01-10 12:34:20 -0200

    Creating Pass LoopStructure

---
+++ lib/Analysis/DepGraph/LoopStructure.h
@@ -0,0 +1,37 @@
+/*
+ * LoopStructure.h
+ *
+ *  Created on: Jan 10, 2014
+ *      Author: raphael
+ */
+
+#ifndef LOOPSTRUCTURE_H_
+#define LOOPSTRUCTURE_H_
+
+#include "llvm/Pass.h"
+
+#include "LoopControllersDepGraph.h"
+#include <set>
+#include <stack>
+
+using namespace std;
+
+namespace llvm {
+
+	class LoopStructure: public llvm::FunctionPass {
+	public:
+		static char ID;
+
+		LoopStructure(): FunctionPass(ID) {}
+		virtual ~LoopStructure() {}
+
+		virtual void getAnalysisUsage(AnalysisUsage &AU) const{
+			AU.addRequired<LoopControllersDepGraph>();
+			AU.addRequired<LoopInfo>();
+			AU.setPreservesAll();
+		}
+
+		bool runOnFunction(Function & F);
+	};
+}
+#endif /* LOOPSTRUCTURE_H_ */
