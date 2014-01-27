45a3f4eb76e0ecdf850457acf4768fb3333514d2
raphael <raphaelernani@gmail.com>
2014-01-27 00:02:52 -0200

    RollBack in TripCountProfiler.sh

--- projects/test-suite/TripCountProfiler.sh
+++ projects/test-suite/TripCountProfiler.sh
@@ -1,14 +1,16 @@
-make clean
-make CC=clang CXX=clang++ TEST=TripCountProfiler report.csv
-
-mv report.TripCountProfiler.csv TripCountProfiler.Vectors.report.csv
+export PATH=/home/raphael/bin:/home/raphael/llvm/Debug+Asserts/bin:/home/raphael/llvm/Debug+Asserts/lib:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/cuda/bin:/home/raphael/pagai3.3/pagai/src:/home/raphael/pagai3.3/pagai/external/z3/bin
 
 make clean
-make CC=clang CXX=clang++ TCFLAGS=-usePericlesTripCount TEST=TripCountProfiler report.csv
+make CC=clang CXX=clang++ TEST=TripCountProfiler report
 
-mv report.TripCountProfiler.csv TripCountProfiler.Pericles.report.csv
+mv report.TripCountProfiler.txt TripCountProfiler.Vectors.report.txt
 
 make clean
-make CC=clang CXX=clang++ TCFLAGS=-useHybridTripCount TEST=TripCountProfiler report.csv
+make CC=clang CXX=clang++ TCFLAGS=-usePericlesTripCount TEST=TripCountProfiler report
 
-mv report.TripCountProfiler.csv TripCountProfiler.Hybrid.report.csv
+mv report.TripCountProfiler.txt TripCountProfiler.Pericles.report.txt
+
+make clean
+make CC=clang CXX=clang++ TCFLAGS=-useHybridTripCount TEST=TripCountProfiler report
+
+mv report.TripCountProfiler.txt TripCountProfiler.Hybrid.report.txtr
